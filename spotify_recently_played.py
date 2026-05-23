3725d2bd1b5842fb8ed7a8215c8bd50c43abcd05
Austin Pesina <55522655+austin-pesina@users.noreply.github.com>
2026-05-23 18:03:05 -0500

    Initial commit of spotify_recently_played.py

---
+++ spotify_recently_played.py
@@ -0,0 +1,44 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import pandas as pd
+import os
+import datetime as dt
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Authenticate Spotify API
+sp = spotipy.Spotify(auth_manager = SpotifyOAuth(
+    client_id = os.getenv('CLIENT_ID'),
+    client_secret = os.getenv('CLIENT_SECRET'),
+    redirect_uri = 'http://127.0.0.1:8080',
+    scope = 'user-read-recently-played'
+))
+
+# Fetch recently played tracks
+results = sp.current_user_recently_played(limit = 50)
+
+song_list = []
+
+# Loop through dictionary and  write to dataframe
+for item in results['items']:
+    track = item['track']
+    song = track['name']
+    artists = ', '.join(artist['name'] for artist in track['artists'])
+    album = track['album']['name']
+    # isrc = track['external_ids']['isrc']
+    played_at = pd.to_datetime(item['played_at']).tz_convert('America/Chicago')
+    
+    song_list.append({
+        'song': song,
+        'artists': artists,
+        'album': album,
+        # 'isrc': isrc,
+        'played_at': played_at
+    })
+
+df = pd.DataFrame(song_list)
+
+print(df.head())
